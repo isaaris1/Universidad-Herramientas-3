7172297e0ac2c3f1736e5f2a86a96fb832989b94
Daniela <daniela.montoya900@pascualbravo.edu.co>
2026-09-21 09:47:14 -0500

    Actualización js

---
+++ app.js
@@ -0,0 +1,75 @@
+class UsuarioModel {
+    constructor() {
+        this.usuarios = [];
+    }
+    agregar(nombre, correo, telefono, metodoPago) {
+        const nuevo = {
+            nombre,
+            correo,
+            telefono,
+            metodoPago
+        };
+        this.usuarios.push(nuevo);
+        return this.usuarios;
+    }
+}
+
+class UsuarioView {
+    constructor() {
+        this.form = document.getElementById('formulario');
+        this.nombre = document.getElementById('nombre');
+        this.correo = document.getElementById('email');
+        this.telefono = document.getElementById('telefono');
+        this.metodoPago = document.getElementById('metodo-pago');
+        this.cuerpoTabla = document.getElementById('cuerpo-tabla');
+    }
+
+    limpiarFormulario() {
+        this.form.reset();
+    }
+
+    mostrarUsuarios(usuarios) {
+        this.cuerpoTabla.innerHTML = ''; 
+
+        usuarios.forEach(usuario => {
+            const fila = `
+                <td>${usuario.nombre}</td>
+                <td>${usuario.correo}</td>
+                <td>${usuario.telefono}</td>
+                <td>${usuario.metodoPago}</td>
+            `;
+            this.cuerpoTabla.innerHTML += fila;
+        });
+    }
+}
+
+class UsuarioController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+
+        this.view.form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            this.procesarFormulario();
+        });
+    }
+
+    procesarFormulario() {
+        const nombre = this.view.nombre.value;
+        const correo = this.view.correo.value;
+        const telefono = this.view.telefono.value;
+        const metodoPago = this.view.metodoPago.value;
+
+        if (nombre && correo && telefono && metodoPago) {
+            const usuariosActualizados = this.model.agregar(nombre, correo, telefono, metodoPago);
+            this.view.mostrarUsuarios(usuariosActualizados);
+            this.view.limpiarFormulario();
+        }
+    }
+}
+
+window.onload = () => {
+    const app = new UsuarioController(new UsuarioModel(), new UsuarioView());
+};
+
+
